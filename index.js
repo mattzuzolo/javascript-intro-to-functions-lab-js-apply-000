4a658d24c11af17e97e6133b866360bf87c236b6
Matt Zuzolo <mattzuzolo@outlook.com>
2018-04-13 16:37:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ function sayHiToGrandma(string) {
     return "I love you, too."
   }
   
-  if ( string === string.toLowerCase() ) {
+  else if ( string === string.toLowerCase() ) {
     return "I can't hear you!";
   }
   
-  if ( string === string.toUpperCase() ) {
+  else if ( string === string.toUpperCase() ) {
     return "YES INDEED!";
   }
   
